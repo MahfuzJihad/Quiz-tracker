92be8104ec5b4d23fad70a307c22fd2e23f36f94
jihadmahfuz <jihadmah166@gmail.com>
2022-10-12 00:45:02 +0600

    added button-card

--- src/component/Topic/Topic.js
+++ src/component/Topic/Topic.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Topic.css';
 
 const Topic = ({ topic }) => {
     const { id, logo, name, total } = topic;
     return (
-        <div>
+        <div className='topic-cart'>
             <img src={logo} alt=""></img>
             <p>{name}</p>
             <p>Tota:{total}</p>
-            <button> <Link to={`/quiz/${id}`}>Start practice</Link> </button>
+            <button> <Link to={`/quiz/${id}`} className='topic-btn'>Start practice</Link> </button>
         </div>
     );
 };
--- src/component/Topics/Topics.js
+++ src/component/Topics/Topics.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Topic from '../Topic/Topic';
+import './Topics.css'
 
 
 const Topics = () => {
     const topics = useLoaderData();
     console.log(topics);
     return (
-        <div>
-            <p>This is topic</p>
+        <div className='topic-container'>
+
             {
                 topics.data.map(topic => <Topic key={topic.id} topic={topic}> </Topic>)
             }
