35a5e9601d91322703fa356b5ce9e7a892b33b12
jihadmahfuz <jihadmah166@gmail.com>
2022-10-12 22:41:17 +0600

    added-header-img

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Header.css';
+import Logo from '../Header/tech.jpg';
 
 
 const Header = () => {
     return (
         <div className='header'>
             <div>
-                <img src="" alt="" />
+                <img src={Logo} width="200" alt="" />
             </div>
-            <div>
+            <div className='quiz-write'>
                 <h2>Check your Tech Knowledge</h2>
                 <p>Put your knowledge about all things technology to the test with our 15 question quiz and have some nerdy fun in the process. Good luck!</p>
             </div>
